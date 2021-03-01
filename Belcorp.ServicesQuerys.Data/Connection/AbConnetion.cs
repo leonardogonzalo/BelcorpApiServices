5f93ed89383cba39a17b831f978e7541b3580dc5
Leonardo Gonzalo Laura <leonarddlg@gmail.com>
2021-03-01 15:26:26 -0500

    commit 2

--- Belcorp.ServicesQuerys.Data/Connection/AbConnetion.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data;
-
-namespace Belcorp.ServicesQuerys.Data.Connection
-{
-    public abstract class AbConnetion
-    {
-        public abstract IDbConnection Conectar(string pais);
-    }
-}
